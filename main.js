8ab9f1efdf8ae0f715fcfd2ded2a69a31e44262a
JaviLlorente <58568865+JaviLlorente@users.noreply.github.com>
2024-01-28 17:11:45 +0100

    Update main.js

--- main.js
+++ main.js
@@ -43,6 +43,8 @@ function init() {
     sidebar.close(panelID);
   });
 
+	map.spin(true, {lines: 13, length: 40}); //on spin
+
   // Use PapaParse to load data from Google Sheets // And call the respective functions to add those to the map.
   Papa.parse(geomURL, {
     download: true,
@@ -155,7 +157,7 @@ function addPoints(data) {
 	
 	// RENDERING METHOD
 	function renderMarkers (data) {
-		map.spin(true, {lines: 13, length: 40}); //on spin
+		
 		pointGroupLayer.clearLayers();
 	 
 		// Choose marker type. Options are: // (these are case-sensitive, defaults to marker!)
