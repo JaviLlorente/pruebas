b06fc5327b46f78bdaf8edc086f8f158fd609554
JaviLlorente <58568865+JaviLlorente@users.noreply.github.com>
2024-02-06 16:56:02 +0100

    Update main.js

--- main.js
+++ main.js
@@ -73,7 +73,7 @@ function addPoints_lista(data) {
 	data = data.data; 
 	var listaEspecies = data;
 	console.log (data);
-	alert("cambiada4");
+	alert("cambiada5");
 }
 
 /////SeleccionandoESPECIE  	
