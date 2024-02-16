c44b232937b38d01d4bcf8471a9b97e3cade253d
JaviLlorente <58568865+JaviLlorente@users.noreply.github.com>
2024-02-16 23:37:37 +0100

    Update main.js

--- main.js
+++ main.js
@@ -258,8 +258,11 @@ function addPoints(data) {
 			//let corte = fechillas.slice(-4);
 			//console.log(corte);
 			
-			const years = fechillas.map( y => new Date( y ).getFullYear() );
+			const years = fechillas.map(y => new Date(y).getFullYear());
 			console.log(years);
+			
+			const min2 = Math.min.apply(null, ((data.map(a => a.Fecha)).map(y => new Date(y).getFullYear())));
+			console.log(min2);
 			
 			//const pieces = fechillas.map( d => d.slice[-4] );
 			//console.log(pieces);
