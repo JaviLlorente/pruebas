7a297609f2e8dd8d26924aeab1fec10df4f9a57d
JaviLlorente <58568865+JaviLlorente@users.noreply.github.com>
2024-02-17 16:09:08 +0100

    Update main.js

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ function init() {
 		scale: 1, // Scales overall size of the spinner
 		corners: 1, // Corner roundness (0..1)
 		speed: 0,5 // Rounds per second
-		rotate: 0, // The rotation offset
+		//rotate: 0, // The rotation offset
 		animation: 'spinner-line-fade-quick', // The CSS animation name for the lines
 		direction: 1, // 1: clockwise, -1: counterclockwise
 		color: '#ffffff', // CSS color or array of colors
