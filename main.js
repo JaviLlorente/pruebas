c2d06ef1780d94cbfaaf8d5f2cd78fa60e58d084
JaviLlorente <58568865+JaviLlorente@users.noreply.github.com>
2024-02-17 16:58:44 +0100

    Update main.js

--- main.js
+++ main.js
@@ -29,22 +29,15 @@ function init() {
   }).addTo(map);
   
 	map.spin(true, {
-		lines: 10, length: 40,
-		width: 16, // The line thickness
-		radius: 25, // The radius of the inner circle
-		scale: 1, // Scales overall size of the spinner
-		corners: 1, // Corner roundness (0..1)
-		speed: 0.7, // Rounds per second
-		rotate: 0, // The rotation offset
-		animation: 'spinner-line-shrink', // The CSS animation name for the lines
-		direction: 1, // 1: clockwise, -1: counterclockwise
+		lines: 12, length: 40, width: 10, radius: 25, speed: 0.7, className: 'spinner',
+		//scale: 1, // Scales overall size of the spinner
+		//corners: 1, // Corner roundness (0..1)
 		//color: '#ffffff', // CSS color or array of colors
 		//fadeColor: 'transparent', // CSS color or array of colors
 		//top: '50%', // Top position relative to parent
 		//left: '50%', // Left position relative to parent
-		shadow: '0 0 1px transparent', // Box-shadow for the lines
-		zIndex: 2000000000, // The z-index (defaults to 2e9)
-		className: 'spinner', // The CSS class to assign to the spinner
+		//shadow: '0 0 1px transparent', // Box-shadow for the lines
+		//zIndex: 2000000000, // The z-index (defaults to 2e9)
 		//position: 'absolute', // Element positioning		
 	}); //on_spin (https://spin.js.org/)
 
@@ -289,7 +282,7 @@ function addPoints(data) {
 			//calcula el array entre min y año en curso (funciona con array de 1 y con número
 			const encurso = new Date().getFullYear()
 			const anios = []
-			for (let i = min2; i <= encurso; i++) { anios.push(i) }
+			for (let i = valormin3; i <= encurso; i++) { anios.push(i) }
 			console.log (anios);
 
 			
