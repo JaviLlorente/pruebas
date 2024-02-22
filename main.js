63ed8ccff4fb91b09df132b95f39e1c3440b3d6d
JaviLlorente <58568865+JaviLlorente@users.noreply.github.com>
2024-02-22 13:06:01 +0100

    Update main.js

--- main.js
+++ main.js
@@ -199,7 +199,7 @@ function addPoints(data) {
 	var slider = document.getElementById("slider");
 	noUiSlider.create(slider, {
 		tooltips: [true, true],
-		behaviour: 'snap',
+		behaviour: 'tap-snap-drag',
 		start: [mini, maxi],
 		connect: true,
 		range: { 'min': mini, 'max': maxi },
