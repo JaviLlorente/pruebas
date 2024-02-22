fe0433c7b7f3a320d423ec38ffcfab01b64967a0
JaviLlorente <58568865+JaviLlorente@users.noreply.github.com>
2024-02-22 18:37:07 +0100

    Update main.js

--- main.js
+++ main.js
@@ -28,6 +28,8 @@ function init() {
 	maxZoom: 18,
   }).addTo(map);
   
+  L.control.zoom({ position: 'bottomright' }).addTo(map);
+  
 	map.spin(true, {
 		lines: 12, length: 40, width: 10, radius: 25, speed: 0.7, className: 'spinner',
 		//scale: 1, // Scales overall size of the spinner
@@ -198,7 +200,7 @@ function addPoints(data) {
 	var slider = document.getElementById("slider");
 	noUiSlider.create(slider, {
 		tooltips: [true, true],
-		behaviour: 'snap-drag',
+		behaviour: 'tap-drag-snap',
 		start: [mini, maxi],
 		connect: true,
 		range: { 'min': mini, 'max': maxi },
