c199e69bb3faf4461a5d6005915542dc9c66bb34
JaviLlorente <58568865+JaviLlorente@users.noreply.github.com>
2024-02-24 13:38:08 +0100

    Update main.js

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ function init() {
 	sidebar = L.control.sidebar({  container: "sidebar", closeButton: true, position: "right",  }).addTo(map);
 	const sidebarElement = document.querySelector('.leaflet-sidebar');
 	sidebarElement.style.marginTop = '1.5em';
-	sidebarElement.style.marginBottom = '0.1em';
+	sidebarElement.style.marginBottom = '0.05em';
   
 	let panelContent = {
 		id: panelID,
