0256cf8502076d5a578bceed678b360fd54a8e7b
JaviLlorente <58568865+JaviLlorente@users.noreply.github.com>
2024-02-25 21:54:46 +0100

    Update main.js

--- main.js
+++ main.js
@@ -205,7 +205,14 @@ function addPoints(data) {
 		orientation: 'horizontal',
 		step: 1,
 		//pips: { mode: 'range', density: 10,	format: { to: function(a){ return pipFormats[a]; } } },
-		pips: { mode: 'values', values: [], density: 9},			
+		pips: { mode: 'values', values: [], density: 9},
+		format: {
+			to: function(value) {
+					// Math.round and -1, so 1.00 => 0, 2.00 => 2, etc.
+					return ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'][Math.round(value) - 1];
+			},
+			from: Number
+		}	
 	});
 	
 	slider.noUiSlider.on('set', function( ) { filterData( ); });
