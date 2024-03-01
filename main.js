fa8c4e974856defc2c9cbebe3155e5e9dd2b79ac
JaviLlorente <58568865+JaviLlorente@users.noreply.github.com>
2024-03-01 21:41:01 +0100

    Update main.js

--- main.js
+++ main.js
@@ -192,7 +192,7 @@ function addPoints(data) {
 			slider.noUiSlider.reset();
 			document.getElementById("claseX").value="-";
 			document.getElementById("especieX").value="-";
-			filterData;
+			filterData();
 			} 
 	};
 	
